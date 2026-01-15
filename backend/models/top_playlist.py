b7346eedeb70b4372b7aee6f448702e98b105223
Illia Kurylenko <sypersys228@gmail.com>
2026-01-15 12:39:35 +0100

    Add top_playlist endpoints and update logic

--- backend/models/top_playlist.py
+++ backend/models/top_playlist.py
@@ -1,8 +1,9 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class TopPlaylist(Base):
     __tablename__ = "top_playlists"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    origin_playlist_id = Column(Integer, ForeignKey("top_playlists.id", ondelete="SET NULL"))
--- backend/services/top_playlist.py
+++ backend/services/top_playlist.py
@@ -40,20 +40,36 @@ class TopPlaylistService:
         return {"id": top_playlist.id, "name": top_playlist.name, "songs": songs_with_votes}
     
     def create_top_playlist(self, name: str, origin_playlist_id: int): 
-        top_playlist = TopPlaylist(name=name)
-        self.db.add(top_playlist)
-        self.db.commit()
-        self.db.refresh(top_playlist)
-
-        songs = self.db.query(Song).filter(Song.playlist_id == origin_playlist_id).all()
+        existent_top_playlist = self.db.query(TopPlaylist).filter(TopPlaylist.origin_playlist_id == origin_playlist_id).first()
+        if existent_top_playlist:
+            songs = self.db.query(Song).filter(Song.playlist_id == origin_playlist_id).all()
         
-        for song in songs:
-            song.top_playlist_id = top_playlist.id
-            self.db.add(song)
-        
-        self.db.commit()
+            for song in songs:
+                vote_count = self.db.query(func.count(Vote.id)).filter(Vote.song_id == song.id).scalar()
+                if vote_count > 0:
+                    song.top_playlist_id = existent_top_playlist.id
+                    self.db.add(song)
 
-        return top_playlist
+            self.db.commit()
+
+            return existent_top_playlist
+
+        else:
+            top_playlist = TopPlaylist(name=name, origin_playlist_id=origin_playlist_id)
+            self.db.add(top_playlist)
+            self.db.commit()
+            self.db.refresh(top_playlist)
+
+            songs = self.db.query(Song).filter(Song.playlist_id == origin_playlist_id).all()
+            
+            for song in songs:
+                vote_count = self.db.query(func.count(Vote.id)).filter(Vote.song_id == song.id).scalar()
+                if vote_count > 0:
+                    song.top_playlist_id = top_playlist.id
+                    self.db.add(song)
+
+            self.db.commit()
+            return top_playlist
     
     def delete_top_playlist(self, top_playlist_id: int):
         top_playlist = self.db.query(TopPlaylist).filter(TopPlaylist.id == top_playlist_id).first()
